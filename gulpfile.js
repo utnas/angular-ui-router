82e5cbfbd037d643b262f684a65632ea7f70b01e
dnsewolo <nsewolo@gmail.com>
2016-04-22 11:14:17 -0400

    Gulp task updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
-var gulp = require('gulp');
-var webserver = require('gulp-webserver');
+var gulp = require('gulp'),
+    connect = require('gulp-connect-multi')();
 
-gulp.task('webserver', function () {
-    gulp.src('app')
-        .pipe(webserver({
-            fallback: 'index.html'
-        }));
+gulp.task('connect', connect.server({
+    root: ['app'],
+    port: 3030,
+    livereload: true,
+    open: {
+        file: 'views/index.html',
+        browser: 'chrome' // if not working OS X browser: 'Google Chrome'
+    }
+}));
+
+gulp.task('html', function () {
+    gulp.src('./app/views/index.html')
+        .pipe(connect.reload());
 });
+
+gulp.task('default', ['connect']);
